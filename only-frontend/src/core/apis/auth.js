a353698f0f23ca327cd7a82f51be4c842df03186
pozafly <pozafly@gmail.com>
2023-06-23 20:19:00 +0900

    chore: remove page/api

--- only-frontend/src/core/apis/auth.js
+++ only-frontend/src/core/apis/auth.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { REQUEST_BODY_TYPE } from '@/constant/auth.js';
 
 const oauthApi = axios.create({
   baseURL: 'https://www.googleapis.com',
@@ -9,36 +8,6 @@ const oauthApi = axios.create({
   params: {},
   timeout: 15 * 1000,
 });
-
-export const getAuthCode = async () => {
-  const response = await axios.get(
-    `${process.env.NEXT_PUBLIC_DOMAIN_URI}/api/auth`
-  );
-
-  return response.data;
-};
-
-export const getAccessToken = async code => {
-  const response = await axios.post(
-    `${process.env.NEXT_PUBLIC_DOMAIN_URI}/api/auth`,
-    {
-      type: REQUEST_BODY_TYPE.GET_TOKEN,
-      code,
-    }
-  );
-  return response.data;
-};
-
-export const getAccessTokenByRefreshToken = async () => {
-  const response = await axios.post(
-    `${process.env.NEXT_PUBLIC_DOMAIN_URI}/api/auth`,
-    {
-      type: REQUEST_BODY_TYPE.GET_TOKEN_BY_REFRESH_TOKEN,
-      accessToken: localStorage.getItem('accessToken'),
-    }
-  );
-  return response.data;
-};
 
 export const getUserInfo = async accessToken => {
   const response = await oauthApi.get(
